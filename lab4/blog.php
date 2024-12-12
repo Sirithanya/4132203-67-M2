8cac24e915b6b4b54de22ef2de53292d2c1b7d7c
Sirithanya <sirithanyai67@gmail.com>
2024-12-12 16:23:02 +0700

    lab4

--- lab4/blog.php
+++ lab4/blog.php
@@ -25,9 +25,33 @@ switch ($method) {
         break;
 
         //curl -X POST -d "title=My Blog&post=This is my first post" http://localhost:8080/php/blog.php
-        
 
+
+    case 'DELETE': // Delete a blog
+        parse_str(file_get_contents("php://input"), $_DELETE);
+        $id = $_DELETE['id'] ?? null;
+
+        if ($id) {
+            $stmt = $conn->prepare("DELETE FROM tb_blog WHERE id = ?");
+            $stmt->bind_param("i", $id);
+
+            if ($stmt->execute()) {
+                $response = ['status' => 'success', 'message' => 'Blog deleted successfully'];
+            } else {
+                $response = ['status' => 'error', 'message' => 'Delete failed: ' . $conn->error];
+            }
+            $stmt->close();
+        } else {
+            $response = ['status' => 'error', 'message' => 'ID is required for deletion'];
+        }
+        break;
+
+        //curl -X DELETE -d "id=1" http://localhost:8080/php/blog.php
+
+    default:
+        $response = ['status' => 'error', 'message' => 'Unsupported request method'];
 }
 
 echo json_encode($response);
 $conn->close();
+
